bcebc30707103f177244d35ffc6f2d1bd53ac897
Florian <dark.joosh@gmail.com>
2025-09-07 15:08:16 +0200

    Add Layout & react router fetch data loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "./ui/Home.jsx";
-import Menu from "./features/menu/Menu.jsx";
+import AppLayout from "./ui/AppLayout.jsx";
+import Error from "./ui/Error.jsx";
+import Menu, { loader as menuLoader} from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
-import Order from "./features/order/Order.jsx";
-import CreateOrder from "./features/order/CreateOrder.jsx";
+import Order, {loader as orderLoader} from "./features/order/Order.jsx";
+import CreateOrder, {action as createOrderAction} from "./features/order/CreateOrder.jsx";
+
 
 const router = createBrowserRouter([
-    { path: '/', element: <Home /> },
-    { path: '/menu', element: <Menu /> },
-    { path: '/cart', element: <Cart /> },
-    { path: '/order/new', element: <CreateOrder /> },
-    { path: '/order/:orderId', element: <Order /> },
+    {
+        element: <AppLayout />,
+        errorElement: <Error />,
+        children: [
+            { path: '/', element: <Home /> },
+            {
+                path: '/menu',
+                element: <Menu />,
+                loader: menuLoader,
+                errorElement: <Error />,
+            },
+            { path: '/cart', element: <Cart /> },
+            {
+                path: '/order/new',
+                element: <CreateOrder />,
+                action: createOrderAction
+            },
+            {
+                path: '/order/:orderId',
+                element: <Order />,
+                loader: orderLoader,
+                errorElement: <Error />,
+            },
+        ]
+    }
 ])
 
 function App() {
-    return (
-        <RouterProvider router={router} />
-    )
+    return  <RouterProvider router={router} />;
 }
 
 export default App
